906d3ba634e665cfa2d932e1c825b9f7a5397082
Aoko <aokodesuka@gmail.com>
2019-11-16 23:34:28 +0800

    增加hooks

---
+++ src/hooks.ts
@@ -0,0 +1,63 @@
+import { useOnMount, useOnUnmount } from "aqua-hooks";
+import { useMemo } from "react";
+import { Bus } from "./bus";
+import { CallbackType, Subscriber } from "./subscriber";
+export interface IUseMessage {
+  [event: string]: CallbackType;
+}
+
+export function useMessage<T extends Bus>(
+  bus: T,
+  event: string,
+  callback: CallbackType
+): Subscriber<T>;
+export function useMessage<T extends Bus>(
+  bus: T,
+  event: IUseMessage | IUseMessage[]
+): Subscriber<T>;
+export function useMessage<T extends Bus>(
+  bus: T,
+  event: string | IUseMessage | IUseMessage[],
+  callback?: CallbackType
+) {
+  const subscriber = useMemo(() => {
+    return bus.createSubscriber();
+  }, [bus]);
+
+  useOnMount(() => {
+    if (typeof event === "string") {
+      if (typeof callback !== "function") {
+        return;
+      }
+      subscriber.on(event, callback);
+    } else if (Array.isArray(event)) {
+      event.forEach(i => {
+        Object.keys(i).forEach(key => {
+          subscriber.on(key, i[key]);
+        });
+      });
+    } else {
+      Object.keys(event).forEach(key => {
+        subscriber.on(key, event[key]);
+      });
+    }
+  });
+
+  useOnUnmount(() => {
+    if (typeof event === "string") {
+      subscriber.off(event);
+    } else if (Array.isArray(event)) {
+      event.forEach(i => {
+        Object.keys(i).forEach(key => {
+          subscriber.off(key);
+        });
+      });
+    } else {
+      Object.keys(event).forEach(key => {
+        subscriber.off(key);
+      });
+    }
+  });
+
+  return subscriber;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Bus } from "./bus";
+import { IUseMessage, useMessage } from "./hooks";
 import { CallbackType, Subscriber } from "./subscriber";
 
 const bus = new Bus();
 
-export { Bus, CallbackType, Subscriber };
+export { Bus, CallbackType, Subscriber, useMessage, IUseMessage };
 export default bus;
